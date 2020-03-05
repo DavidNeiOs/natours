85c294caa17bc8af8818ca77dc3cb95f48cc0ab0
DavidNeiOs <davidneios25@gmail.com>
2020-03-05 18:16:53 -0500

    added card styles

--- src/App.js
+++ src/App.js
@@ -134,6 +134,39 @@ function App() {
             </div>
           </div>
         </section>
+
+        <section className="section-tours">
+          <div className="u-center-text u-margin-bottom-big">
+            <h2 className="heading-secondary">Most popular tours</h2>
+          </div>
+
+          <div className="row">
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front">FRONT</div>
+                <div className="card__side card__side--back card__side--back-1">
+                  BACK
+                </div>
+              </div>
+            </div>
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front">FRONT</div>
+                <div className="card__side card__side--back card__side--back-2">
+                  BACK
+                </div>
+              </div>
+            </div>
+            <div className="col-1-of-3">
+              <div className="card">
+                <div className="card__side card__side--front">FRONT</div>
+                <div className="card__side card__side--back card__side--back-3">
+                  BACK
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
