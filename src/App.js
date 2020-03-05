e446a16623fadb3788243f6fec0661caf23b4720
DavidNeiOs <davidneios25@gmail.com>
2020-03-05 14:39:23 -0500

    text in about section

--- src/App.js
+++ src/App.js
@@ -23,6 +23,44 @@ function App() {
           </a>
         </div>
       </header>
+
+      <main>
+        <section className="section-about">
+          <div className="u-center-text u-margin-bottom-big">
+            <h2 className="heading-secondary">
+              Exciting tours for adventourus people
+            </h2>
+          </div>
+
+          <div className="row">
+            <div className="col-1-of-2">
+              <h3 className="heading-tertiary u-margin-bottom-small">
+                You are going to fall in love with nature
+              </h3>
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                atque distinctio eius veniam nesciunt a voluptatem vitae error
+                fugit doloremque commodi doloribus, minima deleniti ratione
+                inventore architecto ipsam blanditiis rerum!
+              </p>
+              <h3 className="heading-tertiary u-margin-bottom-small">
+                Live adventures like you never have before
+              </h3>
+              <p className="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deserunt, qui reprehenderit? Eligendi ratione quo fuga sequi
+                deleniti laborum magni est in vitae animi, possimus voluptatem!
+                Nobis ex dolor vitae mollitia?
+              </p>
+
+              <a href="#" className="btn-text">
+                Learn more &rarr;
+              </a>
+            </div>
+            <div className="col-1-of-2">image composition</div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
